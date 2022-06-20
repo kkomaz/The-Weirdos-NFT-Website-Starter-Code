59a3dc75748b9eb46c8e3a34977420be3c60a77c
kkomaz <leeajl006@gmail.com>
2022-06-20 17:32:40 -0400

    Navigation styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -17,12 +17,45 @@ const Navbar = styled.nav`
   margin: 0 auto;
 `;
 
+const Menu = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
+`;
+
+const MenuItem = styled.li`
+  margin: 0 1rem;
+  color: ${props => props.theme.text};
+  cursor: pointer;
+
+  &:after {
+    content: ' ';
+    display: block;
+    width: 0%;
+    height: 2px;
+    background: ${props => props.theme.text};
+    transition: width 0.3s ease;
+  }
+
+  &:hover:after {
+    width: 100%;
+  }
+`;
+
 export default function Navigation() {
   return (
     <Section>
       <Navbar>
         <Logo />
-        <h2>Menu</h2>
+        <Menu>
+          <MenuItem>Home</MenuItem>
+          <MenuItem>About</MenuItem>
+          <MenuItem>Roadmap</MenuItem>
+          <MenuItem>Showcase</MenuItem>
+          <MenuItem>Team</MenuItem>
+          <MenuItem>FAQ</MenuItem>
+        </Menu>
         <h2>Button</h2>
       </Navbar>
     </Section>
