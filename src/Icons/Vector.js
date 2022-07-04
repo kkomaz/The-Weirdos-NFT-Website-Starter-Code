2594beff17e238919f109f49a9f892371ecb224c
kkomaz <leeajl006@gmail.com>
2022-07-03 23:30:40 -0400

    Roadmap WIP

--- src/Icons/Vector.js
+++ src/Icons/Vector.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 // import DrawSVGPlugin from "gsap-trial/dist/DrawSVGPlugin";
+// https://react-svgr.com/playground/
 
 const Vector = ({ progress = 0, ...props }) => {
 
--- src/components/DrawSvg.js
+++ src/components/DrawSvg.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Vector from '../Icons/Vector';
 import { useRef, useLayoutEffect } from 'react';
+
+// 2:00:00
 
 export const VectorContainer = styled.div`
   position: absolute;
@@ -20,9 +22,27 @@ export const VectorContainer = styled.div`
   }
 `;
 
+const Bounce = keyframes`
+  from { transform: translateX(-50%) scale(0.5); }
+  to { transform: translateX(-50%) scale(1); }
+`;
+
+export const Ball = styled.div`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 1.5rem;
+  width: 1.5rem;
+  border-radius: 50%;
+  background-color: ${props => props.theme.text};
+  animation: ${Bounce} 0.5s linear infinite alternate;
+`;
 
 export default function DrawSvg() {
   const ref = useRef(null)
+  const ballRef = useRef(null)
+
 
   // Research and learn this
   useLayoutEffect(() => {
@@ -47,18 +67,31 @@ export default function DrawSvg() {
 
           // also reverse the drawing when scroll goes up
           svg.style.strokeDashoffset = length - draw;
+        },
+        onToggle: self => {
+          if (self.isActive) {
+            // console.log('scrolling is active');
+            ballRef.current.style.display = 'none';
+          } else {
+            // console.log('scrolling is not active');
+            ballRef.current.style.display = 'inline-block';
+          }
         }
       }
-    })
+    });
+
     return () => {
-    },
-    []
-  })
+      if (t1) return t1.kill();
+    };
+  }, [])
 
   gsap.registerPlugin(ScrollTrigger)
   return (
-    <VectorContainer ref={ref}>
-      <Vector />
-    </VectorContainer>
+    <>
+      <Ball ref={ballRef} />
+      <VectorContainer ref={ref}>
+        <Vector />
+      </VectorContainer>
+    </>
   );
 }
